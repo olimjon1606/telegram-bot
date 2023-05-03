45908a6975c15811838ef01c8efc61eb760da41e
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-03 23:46:17 +0200

    s

--- bot.py
+++ bot.py
@@ -1,32 +1,32 @@
-# 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
-# import os
-# import openai
-# from aiogram import Bot, Dispatcher, executor, types
+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
+import os
+import openai
+from aiogram import Bot, Dispatcher, executor, types
 
-# bot = Bot(token = '5944753434:AAFVSXlqguHeo6fFWb8pNzIf8NZaGkJKWQA')
-# dp = Dispatcher(bot)
+bot = Bot(token = '5944753434:AAFVSXlqguHeo6fFWb8pNzIf8NZaGkJKWQA')
+dp = Dispatcher(bot)
 
-# openai.api_key = "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"
+openai.api_key = "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"
 
-# @dp.message_handler(commands = ['start', 'help'])
-# async def welcome(message: types.Message):
-#   await message.reply('Hello! Im GPT chat bot. Ask me something')
+@dp.message_handler(commands = ['start', 'help'])
+async def welcome(message: types.Message):
+  await message.reply('Hello! Im GPT chat bot. Ask me something')
 
-# @dp.message_handler()
-# async def gpt(message: types.Message):
-#   response = openai.Completion.create(
-#     model="text-davinci-003",
-#     prompt=message.text,
-#     temperature=0.5,
-#     max_tokens=1024,
-#     top_p=1,
-#     frequency_penalty=0.0,
-#     presence_penalty=0.0
-#   )
-#   await message.reply(response.choices[0].text)
+@dp.message_handler()
+async def gpt(message: types.Message):
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=message.text,
+    temperature=0.5,
+    max_tokens=1024,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+  )
+  await message.reply(response.choices[0].text)
 
-# if __name__ == '__main__':
-#   executor.start_polling(dp)
+if __name__ == '__main__':
+  executor.start_polling(dp)
 
 
 import telegram
