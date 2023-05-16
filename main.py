4ff95a0280d34577e35effcc134b5e33377a8d08
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-16 13:38:02 +0200

    s

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
+from pprint import pprint
 from config import open_weather_token
 
 def get_weather(city, open_weather_token):
     try:
         r = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}"
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
         )
         data = r.json()
-        print(data)
+        pprint(data)
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
     except Exception as ex:
         print(ex)
         print("Please check city name!")
@@ -16,5 +22,5 @@ def main():
     city = input("Enter City: ")
     get_weather(city, open_weather_token)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
