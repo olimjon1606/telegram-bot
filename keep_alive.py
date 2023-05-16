f528160ae518f4cefdf1b71f02b3b11b471c6030
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-16 18:02:03 +0200

    a

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from threading import Thread
+
+@app.route('/')
+def index():
+    # Call your function here to retrieve the desired text
+    text = get_text()
+
+    # Pass the text to the template and render it
+    return render_template('index.html', text=text)
+
+def get_text():
+    # Your logic to retrieve the text goes here
+    return "Hello, world!"
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
--- main_tg.py
+++ main_tg.py
@@ -5,74 +5,75 @@ from config import open_weather_token, tg_bot_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from keep_alive import keep_alive
 
-bot = Bot(token = tg_bot_token)
+bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
+keep_alive()
+
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await message.reply("Enter City name: ")
+  await message.reply("Enter City name: ")
+
 
 @dp.message_handler()
 async def get_weather(message: types.Message):
-    code_to_smile = {
-        "Clear": "Clear \U00002600",
-        "Clouds": "Cloudy \U00002601",
-        "Rain": "Rain \U00002614",
-        "Drizzle": "Drizzle \U00002614",
-        "Thunderstorm": "Thunderstorm \U000026A1",
-        "Snow": "Snow \U0001F328",
-        "Mist": "Mist \U0001F32B"
+  code_to_smile = {
+    "Clear": "Clear \U00002600",
+    "Clouds": "Cloudy \U00002601",
+    "Rain": "Rain \U00002614",
+    "Drizzle": "Drizzle \U00002614",
+    "Thunderstorm": "Thunderstorm \U000026A1",
+    "Snow": "Snow \U0001F328",
+    "Mist": "Mist \U0001F32B"
+  }
 
-    }
+  try:
+    r = requests.get(
+      f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+    )
+    data = r.json()
 
-    try:
-        r = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
-        )
-        data = r.json()
+    city = data["name"]
+    cur_weather = data["main"]["temp"]
 
-        city = data["name"]
-        cur_weather = data["main"]["temp"]
+    weather_desciption = data["weather"][0]["main"]
+    if weather_desciption in code_to_smile:
+      wd = code_to_smile[weather_desciption]
+    else:
+      wd = "Check outside yourself I don't know what weather it is!"
+    humidity = data["main"]["humidity"]
+    pressure = data["main"]["pressure"]
+    wind = data["wind"]["speed"]
+    sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+    sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+    length_of_the_day = datetime.datetime.fromtimestamp(
+      data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+        data["sys"]["sunrise"])
 
-        weather_desciption = data["weather"][0]["main"]
-        if weather_desciption in code_to_smile:
-            wd = code_to_smile[weather_desciption]
-        else:
-            wd = "Check outside yourself I don't know what weather it is!"
-        humidity = data["main"]["humidity"]
-        pressure = data["main"]["pressure"]
-        wind = data["wind"]["speed"]
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
+    random_number = random.randint(1, 9999)
 
-        random_number = random.randint(1, 9999)
+    # Construct the photo URL with the dynamic argument
+    photo_url = f'https://source.unsplash.com/500x400/?{city},{wd}?random={random_number}'
 
-        # Construct the photo URL with the dynamic argument
-        photo_url = f'https://source.unsplash.com/500x400/?{city},{wd}?random={random_number}'
+    # Caption text
+    caption_text = (
+      f"Weather in City:  {city}\nTemperature: {cur_weather}°C {wd}\n"
+      f"Humidity: {humidity}\nPressure: {pressure}\nWind: {wind} m/s\n"
+      f"Sunrise: {sunrise_timestamp}\nSunset: {sunset_timestamp}\nLength of the Day: {length_of_the_day}\n"
+      f"Have Great Day!!!")
 
-        # Caption text
-        caption_text = (f"Weather in City:  {city}\nTemperature: {cur_weather}°C {wd}\n"
-                        f"Humidity: {humidity}\nPressure: {pressure}\nWind: {wind} m/s\n"
-                        f"Sunrise: {sunrise_timestamp}\nSunset: {sunset_timestamp}\nLength of the Day: {length_of_the_day}\n"
-                        f"Have Great Day!!!")
+    # Send photo with caption
+    await bot.send_photo(chat_id=message.chat.id,
+                         photo=photo_url,
+                         caption=caption_text)
 
-        # Send photo with caption
-        await bot.send_photo(
-            chat_id=message.chat.id,
-            photo=photo_url,
-            caption=caption_text
-        )
-
-    except:
-        await message.reply("\U00002620 Please check city name! \U00002620")
-
-
+  except:
+    await message.reply("\U00002620 Please check city name! \U00002620")
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+  executor.start_polling(dp)
 
-    # f"********* {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}  ********\n"
+  # f"********* {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}  ********\n"
