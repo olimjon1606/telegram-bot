30d3689d64f6b5065419ad3e86d9ea0e6874ef8b
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-16 13:09:26 +0200

    Create bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,50 @@
-
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from transformers import pipeline
+from telegram.ext import Updater, CommandHandler
+import requests
 
-# Replace YOUR_BOT_TOKEN with your actual bot token
-bot = telegram.Bot(token='5944753434:AAFVSXlqguHeo6fFWb8pNzIf8NZaGkJKWQA')
-updater = Updater(
-    token='5944753434:AAFVSXlqguHeo6fFWb8pNzIf8NZaGkJKWQA', use_context=True)
+# Set up the Telegram bot token and weather API key
+bot_token = 'YOUR_BOT_TOKEN'
+weather_api_key = 'YOUR_WEATHER_API_KEY'
+
+# Create an instance of the Telegram bot
+bot = telegram.Bot(token=bot_token)
+
+# Define the function to handle the /weather command
+def weather(update, context):
+    # Get the chat ID and the location from the user's message
+    chat_id = update.message.chat_id
+    location = ' '.join(context.args)
+
+    # Send a request to the weather API
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={weather_api_key}'
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract the relevant weather information from the response
+    if data['cod'] == 200:
+        weather_desc = data['weather'][0]['description']
+        temp = data['main']['temp']
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+
+        # Format the weather information into a message
+        message = f'Weather in {location}:\n\n' \
+                  f'Description: {weather_desc}\n' \
+                  f'Temperature: {temp} K\n' \
+                  f'Humidity: {humidity}%\n' \
+                  f'Wind Speed: {wind_speed} m/s'
+    else:
+        message = f'Error retrieving weather information for {location}.'
+
+    # Send the weather information as a reply to the user
+    bot.send_message(chat_id=chat_id, text=message)
+
+# Set up the Telegram bot updater and dispatcher
+updater = Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Hi! I'm ChatGPT, a language model trained by OpenAI. You can chat with me and I'll try to respond as best I can.")
-
-# Define a function for handling text messages
-
-
-def reply(update, context):
-    chat_id = update.effective_chat.id
-    text = update.message.text
-
-    # Use the GPT-3.5 pipeline to generate a response
-    gpt = pipeline('text-generation', model='openai/GPT3.5B', device=0)
-    response = gpt(text, max_length=100)[0]['generated_text']
-
-    # Send the response to the user
-    context.bot.send_message(chat_id=chat_id, text=response)
-
-
-# Add the command and message handlers to the dispatcher
-start_handler = CommandHandler('start', start)
-reply_handler = MessageHandler(Filters.text & (~Filters.command), reply)
-
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(reply_handler)
+# Add the weather command handler
+dispatcher.add_handler(CommandHandler('weather', weather))
 
 # Start the bot
 updater.start_polling()
