323fd8063af464a81b86fd27026c616993de2432
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-19 23:53:27 +0200

    Update main_tg.py

--- main_tg.py
+++ main_tg.py
@@ -76,4 +76,3 @@ async def get_weather(message: types.Message):
 if __name__ == '__main__':
   executor.start_polling(dp)
 
-  # f"********* {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}  ********\n"
