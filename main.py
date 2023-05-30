1399685c2380f5a4342f9063b3c0b544754c468d
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-30 23:53:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,10 +43,8 @@ def get_weather(city, open_weather_token):
     except Exception as ex:
         print(ex)
         print("Please check city name!")
-
 def main():
     city = input("Enter City: ")
     get_weather(city, open_weather_token)
-
 if __name__ == '__main__':
     main()
